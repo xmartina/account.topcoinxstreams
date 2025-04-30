1b01fd2239c00a99ecb5a8da1e15b53ae6ba4b60
xmartina <martinsprince126@gmail.com>
2025-04-30 20:28:47 +0100

    d

--- v_card/include/support_plugin.php
+++ v_card/include/support_plugin.php
@@ -1,7 +1,7 @@
 <!-- Smartsupp Live Chat script -->
 <script type="text/javascript">
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = '727b6e14cb3ee8e977825eeaa8326d758fd05c60';
+_smartsupp.key = '3a7918260ee57bb790835af573ed017da76bb057';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
